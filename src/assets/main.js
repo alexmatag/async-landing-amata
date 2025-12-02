e10c58dee3339fe5e961c27e463ab86009fe9da8
Alejandro Mata <alejandromata@Alejandros-MacBook-Air.local>
2025-12-02 00:42:31 -0600

    [ADD] files

---
+++ src/assets/main.js
@@ -0,0 +1,48 @@
+const API = 'https://images-api.nasa.gov/search?q=galaxy&media_type=image';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'Content-Type': 'application/json'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+        const itemsNASA = await fetchData(API);
+        //console.log(itemsNASA.collection.items);
+
+        let view = `
+            ${itemsNASA.collection.items.map(item => `
+                <div class="group relative">
+                    <div class="w-full aspect-square bg-gray-200 rounded-md overflow-hidden group-hover:opacity-75">
+                        <img 
+                            src="${item.links?.[0]?.href}" 
+                            alt="${item.data?.[0]?.description || item.data?.[0]?.title}" 
+                            class="w-full h-full object-center object-cover" 
+                            onerror="this.onerror=null; this.src='./img/space-banner.png';"
+                        />
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                        <h3 class="text-sm text-slate-200">
+                            <span aria-hidden="true" class="absolute inset-0"></span>
+                            ${item.data?.[0]?.title}
+                        </h3>
+                    </div>
+                </div>
+            `).slice(0, 4).join('')}
+        `;
+        content.innerHTML = view;
+    } catch (error) {
+        console.error(error);
+        alert('Failed to fetch news articles. Please try again later.');
+    }
+})();
